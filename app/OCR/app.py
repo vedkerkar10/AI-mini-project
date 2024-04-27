18342fff885ca33bb85e9c6532281432b23abcd5
vedkerkar10 <113177694+vedkerkar10@users.noreply.github.com>
2024-04-27 22:57:55 +0530

    Delete app directory

--- app/OCR/app.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import pytesseract as tess
-tess.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
-img = Image.open("C:\Users\vedke\Desktop\healthify-me\app\OCR\img.jpg")
-text = tess.image_to_string(img)
-
-print(text)
