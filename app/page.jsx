df6a5b53e8533d6b10775676fd9ca89e55fa4731
vedkerkar10 <113177694+vedkerkar10@users.noreply.github.com>
2024-04-27 23:02:35 +0530

    Add files via upload

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from './_components/Form';
+import Header from './_components/Header';
+
+const Page = () => {
+  return (
+    <div>
+      <Header />
+      <Form />
+    </div>
+  );
+};
+
+export default Page;
